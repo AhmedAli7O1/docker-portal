a08e8917445155663acdb83229c710c3ac9f6ea9
Ahmed Ali <ahmedaliabdullah70@gmail.com>
2017-05-03 16:48:20 +0200

    update app structure

--- index.js
+++
@@ -1,9 +0,0 @@
-const Docker = require('dockerode');
-
-const myDocker = new Docker();
-
-myDocker.listContainers({
-  all: true
-})
-.then(containers => console.log(containers))
-.catch(err => console.log(err));
